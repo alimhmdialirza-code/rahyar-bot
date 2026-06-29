74641d14b991b06cf39aa606c068af13a643dced
alimhmdialirza-code <alimhmdialirza@gmail.com>
2026-06-30 00:12:33 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -232,12 +232,7 @@ def handle_message(message):
 
     if not chat_id:
         return
-if text == "/reset":
-    reset_user(chat_id)
-    users[chat_id] = {"step": "get_name"}
-    send_message(chat_id, "ثبت‌نام مجدد شروع شد!\n\nلطفاً نام خود را وارد کنید:")
-    return
-    
+
     if chat_id not in users:
         users[chat_id] = {"step": "get_name"}
         send_message(chat_id, """سلام! 👋
